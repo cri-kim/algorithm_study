54bb18d2f7d972b3d6f93b2e5e7b89506e3aa007
yonikim <hoyeon.kim2773@gmail.com>
2021-12-07 13:19:33 +0900

    그래프 > 조합

--- yonikim/algorithm-interview/issue_29/combinations.js
+++ yonikim/algorithm-interview/issue_29/combinations.js
@@ -4,4 +4,30 @@
  * @param {number} k
  * @return {number[][]}
  */
-var combine = function (n, k) {};
+var combine = function (n, k) {
+  const array = Array.from({ length: n }, (v, i) => i + 1);
+  //   for (let i = 1; i <= n; i++) {
+  // array.push(i);
+  //   }
+  return getCombinations(array, k);
+};
+
+const getCombinations = (array, selectNumber) => {
+  const result = [];
+  if (selectNumber === 1) return array.map((el) => [el]);
+
+  array.forEach((fixed, index, origin) => {
+    const res = origin.slice(index + 1);
+    const combinations = getCombinations(res, selectNumber - 1);
+    const attached = combinations.map((el) => [fixed, ...el]);
+
+    result.push(...attached);
+  });
+  return result;
+};
+
+const n = 4,
+  k = 2;
+
+const result = combine(n, k);
+console.log(result);
