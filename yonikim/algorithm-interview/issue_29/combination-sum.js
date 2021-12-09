006a6e8a28c23978ba615ff776e94d416ba15929
yonikim <hoyeon.kim2773@gmail.com>
2021-12-09 20:52:30 +0900

    그래프 > 조합의 합

--- yonikim/algorithm-interview/issue_29/combination-sum.js
+++ yonikim/algorithm-interview/issue_29/combination-sum.js
@@ -4,4 +4,24 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function (candidates, target) {};
+var combinationSum = function (candidates, target) {
+  const result = [];
+
+  // Backtracking function to create permutations
+  function permute(arr = [], sum = 0, idx = 0) {
+    if (sum > target) return;
+    if (sum === target) result.push(arr);
+
+    // Start i at idx to avoid using the same combination of numbers but in a different order
+    for (let i = idx; i < candidates.length; i++) {
+      permute([...arr, candidates[i]], sum + candidates[i], i);
+    }
+  }
+  permute();
+  return result;
+};
+
+const candidates = [2, 3, 6, 7],
+  target = 7;
+const result = combinationSum(candidates, target);
+console.log(result);
