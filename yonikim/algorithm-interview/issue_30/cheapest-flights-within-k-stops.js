72d76a3fca4e236536ec6d228c52f50f4bea2e8f
yonikim <hoyeon.kim2773@gmail.com>
2021-12-15 14:05:00 +0900

    최단 경로 문제

--- yonikim/algorithm-interview/issue_30/cheapest-flights-within-k-stops.js
+++ yonikim/algorithm-interview/issue_30/cheapest-flights-within-k-stops.js
@@ -7,4 +7,51 @@
  * @param {number} k
  * @return {number}
  */
-var findCheapestPrice = function (n, flights, src, dst, k) {};
+var findCheapestPrice = function (n, flights, src, dst, k) {
+  // 인접 리스트 생성
+  const adj = new Map();
+  for (let [frm, to, weight] of flights) {
+    if (adj.has(frm)) adj.get(frm).push([to, weight]);
+    else adj.set(frm, [[to, weight]]);
+  }
+
+  function dijkstra(start) {
+    let q = [];
+    let dist = new Array(n).fill(Infinity);
+
+    q.push([start, 0, 0]);
+    dist[start] = 0;
+
+    while (q.length) {
+      let [here, cost, move] = q.shift();
+
+      if (move > K || !adj.get(here)) continue;
+
+      for (let [there, weight] of adj.get(here)) {
+        let nextDist = cost + weight;
+        if (dist[there] > nextDist) {
+          dist[there] = nextDist;
+          q.push([there, nextDist, move + 1]);
+        }
+      }
+    }
+
+    return dist[dst];
+  }
+
+  let ans = dijkstra(src);
+  return ans === Infinity ? -1 : ans;
+};
+
+const n = 3;
+const flights = [
+  [0, 1, 100],
+  [1, 2, 100],
+  [0, 2, 500],
+];
+const src = 0;
+const dst = 2;
+const k = 1;
+
+const result = findCheapestPrice(n, flights, src, dst, k);
+console.log(result);
--- yonikim/algorithm-interview/issue_30/network-delay-time.js
+++ yonikim/algorithm-interview/issue_30/network-delay-time.js
@@ -6,3 +6,14 @@
  * @return {number}
  */
 var networkDelayTime = function (times, n, k) {};
+
+const times = [
+    [2, 1, 1],
+    [2, 3, 1],
+    [3, 4, 1],
+  ],
+  n = 4,
+  k = 2;
+
+const result = networkDelayTime(times, n, k);
+console.log(result);
