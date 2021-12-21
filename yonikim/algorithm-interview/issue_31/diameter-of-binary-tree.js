0629a829870a54fa95a4f4d54ac94b85df8ca5d5
yonikim <hoyeon.kim2773@gmail.com>
2021-12-21 15:15:36 +0900

    트리 initial file setting

---
+++ yonikim/algorithm-interview/issue_31/diameter-of-binary-tree.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/diameter-of-binary-tree/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {};
---
+++ yonikim/algorithm-interview/issue_31/minimum-height-trees.js
@@ -0,0 +1,7 @@
+/**
+ * https://leetcode.com/problems/minimum-height-trees/
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findMinHeightTrees = function (n, edges) {};
