062ce7746ad685c32579f1df7b522662f04ba31b
yonikim <hoyeon.kim2773@gmail.com>
2022-01-02 16:42:16 +0900

    트리

--- yonikim/algorithm-interview/issue_31/range-sum-of-bst.js
+++ yonikim/algorithm-interview/issue_31/range-sum-of-bst.js
@@ -13,4 +13,25 @@
  * @param {number} high
  * @return {number}
  */
-var rangeSumBST = function (root, low, high) {};
+var rangeSumBST = function (root, low, high) {
+  if (!root) return 0;
+  let sum = 0;
+  if (root.val >= low && root.val <= high) {
+    sum = root.val;
+  }
+
+  if (root.val >= low) {
+    sum += rangeSumBST(root.left, low, high);
+  }
+  if (root.val <= high) {
+    sum += rangeSumBST(root.right, low, high);
+  }
+
+  return sum;
+};
+
+const root = [10, 5, 15, 3, 7, null, 18],
+  low = 7,
+  high = 15;
+const result = rangeSumBST(root, low, high);
+console.log(result);
--- yonikim/algorithm-interview/issue_31/serialize-and-deserialize-binary-tree.js
+++ yonikim/algorithm-interview/issue_31/serialize-and-deserialize-binary-tree.js
@@ -13,7 +13,28 @@
  * @param {TreeNode} root
  * @return {string}
  */
-var serialize = function (root) {};
+var serialize = function (root) {
+  if (!root) return null;
+
+  const data = [];
+  const queue = [root];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (node) {
+      data.push(node.val);
+
+      queue.push(node.left);
+      queue.push(node.right);
+    } else {
+      data.push(null);
+    }
+  }
+
+  while (data.length > 0 && data[data.length - 1] === null) {
+    data.pop();
+  }
+  return JSON.stringify(data);
+};
 
 /**
  * Decodes your encoded data to tree.
@@ -21,9 +42,43 @@ var serialize = function (root) {};
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function (data) {};
+var deserialize = function (data) {
+  try {
+    data = JSON.parse(data);
+  } catch (err) {
+    return null;
+  }
+
+  if (!(data instanceof Array) || data.length === 0) {
+    return null;
+  }
+
+  const root = new TreeNode(data.shift());
+  const queue = [root];
+
+  while (data.length > 0) {
+    const node = queue.shift();
+
+    let val = data.shift();
+    if (typeof val !== "undefined" && val !== null) {
+      node.left = new TreeNode(val);
+      queue.push(node.left);
+    }
+
+    val = data.shift();
+    if (typeof val !== "undefined" && val !== null) {
+      node.right = new TreeNode(val);
+      queue.push(node.right);
+    }
+  }
+  return root;
+};
 
 /**
  * Your functions will be called as such:
  * deserialize(serialize(root));
  */
+
+const root = [1, 2, 3, null, null, 4, 5];
+const result = serialize(root);
+console.log(result);
